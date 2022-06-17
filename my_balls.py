1821a32f0536c7d481171c8b0a4e47571a6a6b27
Ditya4 <72632156+Ditya4@users.noreply.github.com>
2022-06-17 15:52:25 +0300

    refactor change colors by mouse pressed

--- my_balls.py
+++ my_balls.py
@@ -4,7 +4,7 @@ from random import randint
 
 class Ball:
 
-    def __init__(self, x, y, dx, dy, radius, r, g, b, surface):
+    def __init__(self, x, y, dx, dy, radius, r, dr, g, dg, b, db, surface):
         """ x, y are a coords of centr of the circle.
             x >= 0 and x <= :WINDOW_WIDTH
             y >= 0 and y <= :WINDOW_HEIGHT
@@ -13,6 +13,7 @@ class Ball:
             if we don't we'll get a stuck ball
             radius > 0 and radius < min(WINDOW_WIDTH, WINDOW_HEIGHT)
             r, g ,b are a color components (r, g, b)
+            dr, db, dg - are steps to change color component
         """
         if dx == dy == 0:
             dx = randint(1, 5)
@@ -23,8 +24,11 @@ class Ball:
         self.dy = dy
         self.radius = radius
         self.r = r
+        self.dr = dr
         self.g = g
+        self.dg = dg
         self.b = b
+        self.db = db
         self.surface = surface
 
     def move(self):
@@ -48,14 +52,29 @@ class Ball:
                 return True
         return False
 
+    def check_collision(self, ball):
+        return False
+
+
+def change_color_component(color_component, d_color_component):
+    if 10 + d_color_component < color_component + d_color_component < (
+                                                    245 - d_color_component):
+        color_component += d_color_component
+    else:
+        d_color_component *= -1
+        color_component += d_color_component
+    return color_component, d_color_component
+
 
 def main():
     """ lowe_r and upper_r is a color part r in RGB model random range, should
         be between 1 and 254
     """
-    lower_r = 6
-    upper_r = 249
+    lower = 6
+    upper = 249
     dr = 4
+    dg = 4
+    db = 4
     g_fill = 245
     dg_fill = 2
     b_fill = 245
@@ -72,49 +91,46 @@ def main():
         balls.append(Ball(randint(BALL_RADIUS, WINDOW_WIDTH - BALL_RADIUS),
                           randint(BALL_RADIUS, WINDOW_HEIGHT - BALL_RADIUS),
                           randint(-3, 3), randint(-3, 3), BALL_RADIUS,
-                          randint(lower_r, upper_r), randint(lower_r, upper_r),
-                          randint(lower_r, upper_r), surface))
+                          randint(lower, upper), dr,
+                          randint(lower, upper), dg,
+                          randint(lower, upper), db, surface))
     while run:
         clock.tick(45)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
         if pygame.mouse.get_pressed(num_buttons=3)[0]:
+            any_ball_clicked = False
             for ball in balls:
                 if ball.is_clicked(*pygame.mouse.get_pos()):
-                    if 5 <= ball.r <= 248:
-                        ball.r += dr
-                    else:
-                        dr *= -1
-                        ball.r += dr
-                    ball.color = (ball.r, ball.g, ball.b)
-                else:
-                    if db_fill + 10 < b_fill + db_fill < 248 - db_fill:
-                        b_fill += db_fill
-                    else:
-                        db_fill *= -1
-                        b_fill += db_fill
+                    ball.r, ball.dr = change_color_component(ball.r, ball.dr)
+                    any_ball_clicked = True
+            if not any_ball_clicked:
+                r_fill, dr_fill = change_color_component(r_fill, dr_fill)
+                ball.color = (ball.r, ball.g, ball.b)
+
         if pygame.mouse.get_pressed(num_buttons=3)[2]:
+            any_ball_clicked = False
             for ball in balls:
                 if ball.is_clicked(*pygame.mouse.get_pos()):
-                    if 5 <= ball.r <= 250:
-                        ball.r -= dr
-                    else:
-                        dr *= -1
-                        ball.r -= dr
-                    ball.color = (ball.r, ball.g, ball.b)
-                else:
-                    if dg_fill < g_fill + dg_fill < 255 - dg_fill:
-                        g_fill += dg_fill
-                    else:
-                        dg_fill *= -1
-                        g_fill += dg_fill
+                        ball.g, ball.dg = change_color_component(ball.g,
+                                                                 ball.dg)
+                        any_ball_clicked = True
+            if not any_ball_clicked:
+                g_fill, dg_fill = change_color_component(g_fill, dg_fill)
+                ball.color = (ball.r, ball.g, ball.b)
+
         if pygame.mouse.get_pressed(num_buttons=3)[1]:
-            if dr_fill + 10 < r_fill + dr_fill < 249 - dr_fill:
-                r_fill += dr_fill
-            else:
-                dr_fill *= -1
-                r_fill += dr_fill
+            any_ball_clicked = False
+            for ball in balls:
+                if ball.is_clicked(*pygame.mouse.get_pos()):
+                    ball.b, ball.db = change_color_component(ball.b, ball.db)
+                    any_ball_clicked = True
+            if not any_ball_clicked:
+                b_fill, db_fill = change_color_component(b_fill, db_fill)
+                ball.color = (ball.r, ball.g, ball.b)
+
         surface.fill((r_fill, g_fill, b_fill,))
         for ball in balls:
             ball.move()
